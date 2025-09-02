76f510b1d698582fceccebba45dbe668a2dcd009
Michał Kaniowski <kaniowskimichal2@gmail.com>
2025-09-02 16:39:25 +0200

    fix: fix incorrect mutation of cache data after creating task

--- src/features/tasks/hooks/use-create-task.ts
+++ src/features/tasks/hooks/use-create-task.ts
@@ -1,8 +1,8 @@
-import { Task } from "@/generated/prisma";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { createTask } from "../actions/create-task";
 import { taskQueryKeys } from "../task-query-key-factory";
+import { ColumnWithTasks } from "./../../../components/kanban-board2";
 
 export const useCreateTask = ({ projectId }: { projectId: string }) => {
   const queryClient = useQueryClient();
@@ -13,17 +13,28 @@ export const useCreateTask = ({ projectId }: { projectId: string }) => {
     onSuccess: async (task) => {
       await queryClient.cancelQueries({ queryKey });
 
-      const previousState = queryClient.getQueryData<{ tasks: Task[] }>(
-        queryKey
+      const previousState = queryClient.getQueryData<{
+        columnsWithTasks: ColumnWithTasks[];
+      }>(queryKey);
+
+      queryClient.setQueryData<{ columnsWithTasks: ColumnWithTasks[] }>(
+        queryKey,
+        (prev) => {
+          if (!prev) return { columnsWithTasks: [] };
+
+          return {
+            columnsWithTasks: prev.columnsWithTasks.map((column) => {
+              if (column.id === task.statusId) {
+                return {
+                  ...column,
+                  tasks: [...column.tasks, task],
+                };
+              }
+              return column;
+            }),
+          };
+        }
       );
-
-      queryClient.setQueryData<{ tasks: Task[] }>(queryKey, (prev) => {
-        if (!prev) return { tasks: [task] };
-
-        return {
-          tasks: [...prev.tasks, task],
-        };
-      });
 
       toast.success("Task created successfully");
 
